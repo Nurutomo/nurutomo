fe3990af8122da875fdc25bef154a398c986d956
Nurutomo <tixbig@gmail.com>
2020-11-27 10:31:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -6,6 +7,47 @@ const app = express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
+  .get('/api/ssweb', async (req, res) => {
+	  try {
+		let { url, full, delay } = {
+			url: '',
+			full: false,
+			delay: 0,
+			...req.query
+		}
+		if (url) {
+			const page = await browser.newPage()
+			await page.goto(url, {
+				waitUntil: 'load',
+				timeout: 300000
+			})
+			if (delay > 0) await sleep(delay)
+			const screenshot = await page.screenshot({
+				type: 'png',
+				encoding: 'base64',
+				fullPage: !!full
+			})
+			res.writeHead(200, {
+				'Content-Type': 'image/png',
+				'Content-Length': screenshot.length
+			});
+			res.end(screenshot);
+		} else {
+			res.status(501).json({
+				error: 'parameter \'url\' not provided',
+				status: 501
+			})
+		}
+	  } catch (e) {
+		res.status(501).json({
+			error: e,
+			status: 501
+		})
+	  }
+  })
+  .get('*', function(req, res){
+  	res.status(404).send('404 Not Found')
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 const io = require('socket.io')(app);
 
@@ -19,3 +61,26 @@ io.on('connection', (socket) => {
 	
 	socket.emit('0', 'OOF')
 })
+
+(async () => {
+	const chromeOptions = {
+    		headless: true,
+    		defaultViewport: {
+            		width: 1920,
+            		height: 1080
+        	},
+        	timeout: 120000
+    		args: [
+        		"--incognito",
+        		"--no-sandbox",
+        		"--single-process",
+        		"--no-zygote"
+    		],
+	}
+	global.browser = await puppeteer.launch(chromeOptions);
+    	console.log('Browser Launched')
+})()
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
